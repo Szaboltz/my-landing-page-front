938fe5a41a127679918d77039d59706ba06e3830
Henrique Szabo <szabopessoal@gmail.com>
2024-04-03 13:58:27 -0300

    Add: Content component

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,40 +1,22 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-import perfil from "./assets/perfil.jpg"
+import perfil from "./assets/perfil.jpg";
+import './App.css'
 
-
-function AboutMe(){
-  const text: string = "Olá, sou Henrique, um desenvolvedor Full Stack dedicado e apaixonado por tecnologia. Minha experiência abrange tanto o desenvolvimento do lado do servidor quanto a criação de interfaces interativas do lado do cliente. Com habilidades sólidas em linguagens como JavaScript e expertise em frameworks modernos, estou comprometido em entregar soluções eficientes e de alta qualidade. Tenho um histórico de sucesso na criação de aplicativos web inovadores e estou constantemente buscando desafios para aprimorar minhas habilidades. Se você procura um profissional comprometido com a excelência técnica e resultados excepcionais, estou pronto para contribuir ao seu projeto"
-
-
-  return(
-    <Card className="rounded-none mt-6">
-      <CardContent className="p-4 grid gap-6 grid-cols-2">
-        <Card>
-          <CardHeader>
-            <CardTitle>About me</CardTitle>
-          </CardHeader>
-          <CardContent className="text-justify">
-            <p className="w-full">
-              {text}
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>It's Me!</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <img src={perfil} alt="Profile Photo" />
-          </CardContent>
-        </Card>
-      </CardContent>
-    </Card>
-  )
+function AboutMe() {
+  return (
+    <div className="bg-my-grey flex justify-center h-500 p-10">
+      <div className="grid grid-cols-2 gap-5 h-full">
+        <div className="flex flex-col justify-center itens-center">
+          <h2>About me</h2>
+          <p>
+            "Olá, sou Henrique, um desenvolvedor Full Stack dedicado e apaixonado por tecnologia. Minha experiência abrange tanto o desenvolvimento do lado do servidor quanto a criação de interfaces interativas do lado do cliente. Com habilidades sólidas em linguagens como JavaScript e expertise em frameworks modernos, estou comprometido em entregar soluções eficientes e de alta qualidade. Tenho um histórico de sucesso na criação de aplicativos web inovadores e estou constantemente buscando desafios para aprimorar minhas habilidades. Se você procura um profissional comprometido com a excelência técnica e resultados excepcionais, estou pronto para contribuir ao seu projeto" 
+          </p>
+        </div>
+          <div className="flex justify-end items-center">
+            <img src={ perfil } alt="" className="fit-img"/>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
