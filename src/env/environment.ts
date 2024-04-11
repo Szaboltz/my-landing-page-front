448927cddd7dbbfe086087b061063bf19ad35b85
Henrique Szabo <szabopessoal@gmail.com>
2024-04-11 14:28:04 -0300

    Add: Favicon and curriculum

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,6 +1,6 @@
 const environment = {
   url: "http://localhost:8080",
-  pdf: "https://drive.google.com/uc?export=download&id=1YALuuSPuHHE5aI27x3s0fKk2duHLi4nT",
+  pdf: "https://drive.google.com/uc?export=download&id=1vSJPp52LUuOazOVmkpTVNN9hicmG5HWO",
 }
 
 export default environment
