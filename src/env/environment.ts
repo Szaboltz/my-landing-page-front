ac0f4c9da1dec473c9fd385a955fd07f604c411b
Henrique Szabo <szabopessoal@gmail.com>
2024-04-15 12:31:04 -0300

    Fix: URL API

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,5 +1,5 @@
 const environment = {
-  url: "http://localhost:8080",
+  url: "https://damp-tor-78404-b8d20f6596c9.herokuapp.com",
   pdf: "https://drive.google.com/uc?export=download&id=1hyWUHtwDO3XPxb4FrfJt6Y-W02zw9xYh",
 }
 
