97461203034724e3c12891cd950fe555cb36cfaf
Henrique Szabo <szabopessoal@gmail.com>
2024-04-30 20:44:56 -0300

    Deploy

--- src/NavContent.tsx
+++ src/NavContent.tsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
 import environment from "./env/environment";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogDescription,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-//   DialogFooter
-// } from "@/components/ui/dialog";
-// import { Label } from "@/components/ui/label";
-// import { Input } from "@/components/ui/input";
-// import { Textarea } from "@/components/ui/textarea";
-// import { useState } from "react";
-// import { ReloadIcon } from "@radix-ui/react-icons";
-// import toast, { Toaster } from "react-hot-toast";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import toast, { Toaster } from "react-hot-toast";
 
 function NavContent() {
   const links: any[] = [
@@ -27,121 +27,121 @@ function NavContent() {
     { name: "Contact me", href: "?#" },
   ];
 
-  // const [formData, setFormData] = useState({
-  //   name: "",
-  //   email: "",
-  //   message: "",
-  // });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  // const [emptyData, setEmptyData] = useState(false);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [nameError, setNameError] = useState(false);
-  // const [emailError, setEmailError] = useState(false);
-  // const [messageError, setMessageError] = useState(false);
+  const [emptyData, setEmptyData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
 
-  // const handleChange = (e: any) => {
-  //   const { name, value } = e.target;
-  //   setFormData((prevState) => ({
-  //     ...prevState,
-  //     [name]: value,
-  //   }));
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
 
-  //   if (name == "name") {
-  //     if (value.length > 0 && value.length < 3) {
-  //       setNameError(true);
-  //     } else {
-  //       setNameError(false);
-  //     }
-  //     if (value.length > 0) setEmptyData(false);
-  //   }
+    if (name == "name") {
+      if (value.length > 0 && value.length < 3) {
+        setNameError(true);
+      } else {
+        setNameError(false);
+      }
+      if (value.length > 0) setEmptyData(false);
+    }
 
-  //   if (name == "email") {
-  //     if (!validateEmail(value) && value.length > 0) {
-  //       setEmailError(true);
-  //     } else {
-  //       setEmailError(false);
-  //     }
-  //     if (value.length > 0) setEmptyData(false);
-  //   }
+    if (name == "email") {
+      if (!validateEmail(value) && value.length > 0) {
+        setEmailError(true);
+      } else {
+        setEmailError(false);
+      }
+      if (value.length > 0) setEmptyData(false);
+    }
 
-  //   if (name == "message") {
-  //     if (name == "message" && value.length > 0 && value.length < 3) {
-  //       setMessageError(true);
-  //     } else {
-  //       setMessageError(false);
-  //     }
-  //     if (value.length > 0) setEmptyData(false);
-  //   }
-  // };
+    if (name == "message") {
+      if (name == "message" && value.length > 0 && value.length < 3) {
+        setMessageError(true);
+      } else {
+        setMessageError(false);
+      }
+      if (value.length > 0) setEmptyData(false);
+    }
+  };
 
-  // const validateEmail = (email: string) => {
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   return emailRegex.test(email);
-  // };
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
-  // const save = async (event: any) => {
-  //   let toSafe: boolean = true;
-  //   event.preventDefault();
+  const save = async (event: any) => {
+    let toSafe: boolean = true;
+    event.preventDefault();
 
-  //   if (
-  //     formData.email.length <= 0 ||
-  //     formData.name.length <= 0 ||
-  //     formData.message.length <= 0
-  //   ) {
-  //     toSafe = false;
-  //     setEmptyData(true);
-  //     toast.error("Fill in the required fields (*)", {
-  //       duration: 3000,
-  //     });
-  //   }
+    if (
+      formData.email.length <= 0 ||
+      formData.name.length <= 0 ||
+      formData.message.length <= 0
+    ) {
+      toSafe = false;
+      setEmptyData(true);
+      toast.error("Fill in the required fields (*)", {
+        duration: 3000,
+      });
+    }
 
-  //   if (nameError || emailError || messageError) {
-  //     toSafe = false;
-  //     toast.error("There are fields with incorrect entries.", {
-  //       duration: 3000,
-  //     });
-  //   }
+    if (nameError || emailError || messageError) {
+      toSafe = false;
+      toast.error("There are fields with incorrect entries.", {
+        duration: 3000,
+      });
+    }
 
-  //   if (toSafe) {
-  //     setIsLoading(true);
-  //     try {
-  //       await fetch(`${environment.url}api/send`, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(formData),
-  //       }).then((response: any) => {
-  //         if (!response.ok) {
-  //           toast.error(
-  //             "Failed to send message. Please check your network connection.",
-  //             {
-  //               duration: 3000,
-  //             }
-  //           );
-  //           throw new Error("Network response was not ok.");
-  //         }
-  //         setFormData({
-  //           name: "",
-  //           email: "",
-  //           message: "",
-  //         });
-  //         setIsLoading(false);
-  //         toast.success("Message send sucefully", {
-  //           duration: 3000,
-  //         });
-  //       });
-  //     } catch (e) {
-  //       setIsLoading(false);
-  //       toast.error(
-  //         "Failed to send message. Please check your network connection.",
-  //         {
-  //           duration: 3000,
-  //         }
-  //       );
-  //     }
-  //   }
-  // };
+    if (toSafe) {
+      setIsLoading(true);
+      try {
+        await fetch(`${environment.url}/api/send`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }).then((response: any) => {
+          if (!response.ok) {
+            toast.error(
+              "Failed to send message. Please check your network connection.",
+              {
+                duration: 3000,
+              }
+            );
+            throw new Error("Network response was not ok.");
+          }
+          setFormData({
+            name: "",
+            email: "",
+            message: "",
+          });
+          setIsLoading(false);
+          toast.success("Message send sucefully", {
+            duration: 3000,
+          });
+        });
+      } catch (e) {
+        setIsLoading(false);
+        toast.error(
+          "Failed to send message. Please check your network connection.",
+          {
+            duration: 3000,
+          }
+        );
+      }
+    }
+  };
 
   return (
     <div className="text-white sm:mr-8">
@@ -167,7 +167,103 @@ function NavContent() {
             </a>
           </Button>
         </li>
+        <Dialog>
+          <li>
+            <DialogTrigger>
+              <Button variant="ghost" className="font-medium text-base">
+                {links[3].name}
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Send me a message</DialogTitle>
+                <DialogDescription>What's the big deal?</DialogDescription>
+              </DialogHeader>
+              <form onSubmit={save} className="space-y-6">
+                <div>
+                  <Label htmlFor="name">
+                    Name <span className="text-xs text-red-600">*</span>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Bob"
+                  />
+                  {nameError && (
+                    <span className="text-xs text-red-600">
+                      Name must be at least 3 characters
+                    </span>
+                  )}
+                  {emptyData && formData.name.length == 0 && (
+                    <span className="text-xs text-red-600">
+                      Fill in this field
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <Label htmlFor="email">
+                    Email <span className="text-xs text-red-600">*</span>
+                  </Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="bob@gmail.com"
+                  />
+                  {emailError && (
+                    <span className="text-xs text-red-600">
+                      Email must be valid
+                    </span>
+                  )}
+                  {emptyData && formData.email.length == 0 && (
+                    <span className="text-xs text-red-600">
+                      Fill in this field
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <Label htmlFor="message">
+                    Message <span className="text-xs text-red-600">*</span>
+                  </Label>
+                  <Textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder="Type something"
+                  />
+                  {messageError && (
+                    <span className="text-xs text-red-600">
+                      Message must be at least 3 characters
+                    </span>
+                  )}
+                  {emptyData && formData.message.length == 0 && (
+                    <span className="text-xs text-red-600">
+                      Fill in this field
+                    </span>
+                  )}
+                </div>
+                <DialogFooter>
+                  {isLoading ? (
+                    <Button disabled>
+                      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                      Please wait
+                    </Button>
+                  ) : (
+                    <Button type="submit">Send</Button>
+                  )}
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </li>
+        </Dialog>
       </ul>
+      <Toaster />
     </div>
   );
 }
--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,5 +1,5 @@
 const environment = {
-  url: "https://my-landing-page-back.onrender.com/",
+  url: "https://my-landing-page-back.onrender.com",
   pdf: "https://drive.google.com/uc?export=download&id=1hyWUHtwDO3XPxb4FrfJt6Y-W02zw9xYh",
 }
 
