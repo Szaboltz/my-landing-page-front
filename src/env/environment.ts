930008275c13ff22aabb37c5bc75eacc826c138d
Henrique Szabo <szaboprofissional@gmail.com>
2025-01-06 19:33:42 -0300

    cv update

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,6 +1,6 @@
 const environment = {
   url: "https://my-landing-page-back.onrender.com",
-  pdf: "https://drive.google.com/uc?export=download&id=1r-QAc7h6Z-n2zPs8Bb6NnvHv_nRhywax",
+  pdf: "https://drive.google.com/uc?export=download&id=1B2WJwlVnhzfIK-lY1MeTW20rcynAtmBw",
 }
 
 export default environment
